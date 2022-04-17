afcd78dd0ab029cbc3a626a032f6028232ea8ebf
noodleween <happyguy122x@gmail.com>
2022-04-17 17:06:55 -0600

    CB-23 Add: Testing to transaction

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from functions.aline_user_generator import generate_admin, get_member_json
 from functions.aline_applicant_generator import generate_applicant
 from functions.aline_application_generator import generate_application, process_applicants
 from functions.aline_bank_and_branches_generator import generate_bank_address, generate_branch_address
-from functions.aline_transactions_generator import deposit, withdrawal, payment
+from functions.aline_transactions_generator import deposit, withdrawal, payment, transfer
 # to run microservice  mvn spring-boot:run -pl {microservice dir}
 
 
 def main():
     # run users and bank microservice (gateway can be run for faster result)
     print(generate_admin())
-    print(get_member_json(1))
+    print(get_member_json())
     # run users microservice (gateway can be run for faster result)
     # run underwriter microservice
     print(generate_application())
@@ -19,9 +19,10 @@ def main():
     print(generate_bank_address())
     print(generate_branch_address())
     # run transaction and account microservice
-    print(deposit())
-    print(withdrawal())
-    print(payment())
+    deposit(1, 10000)
+    withdrawal(1, 600)
+    payment(1, 100)
+    print(transfer(1, 2, 3, 1000))
 
 
 if __name__ == '__main__':
